2afb6f6442c3dfd2dc6ef4ca9902adeef695c43d
Vitalis Mutharimi <63202472+VitalSalie@users.noreply.github.com>
2020-08-11 23:34:12 +0300

    Add files via upload

---
+++ contact.1.js
@@ -0,0 +1,34 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+  
+  /* Open */
+  function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+  
+  
+  /* Open */
+  function openNav() {
+    document.getElementById("myNav").style.display = "block";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
+
+
+
