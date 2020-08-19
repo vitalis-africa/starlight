e196f581c06d7db904a7cab8d14ca5763a50b42e
exit <vitalyonchi@gmail.com>
2020-08-20 00:46:51 +0300

    web update

--- index.1.js
+++ index.1.js
@@ -1,31 +1,25 @@
-/* Open when someone clicks on the span element */
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
   }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
-  
-  
-  /* Open */
   function openNav() {
     document.getElementById("myNav").style.height = "100%";
   }
-  
-  /* Close */
   function closeNav() {
     document.getElementById("myNav").style.height = "0%";
   }
-  
-  
-  /* Open */
   function openNav() {
     document.getElementById("myNav").style.display = "block";
   }
-  
-  /* Close */
   function closeNav() {
     document.getElementById("myNav").style.display = "none";
+  }
+
+
+document.getElementById("tosh").onclick=displaytwitterhandle;
+
+  function displaytwitterhandle(){
+      document.getElementById("twitterhandle").innerHTML=Date()
   }
